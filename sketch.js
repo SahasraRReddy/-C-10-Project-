c9865acd634162c7ba74c8d002748d4e08a938e9
SahasraRReddy <114499019+SahasraRReddy@users.noreply.github.com>
2022-10-16 09:08:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+ var weights = [20, 30, 50, 80, 90]
+ var sum = weights[0]+weights[1]+weights[2]+weights[3]+weights[4]
+ var average = sum/weights.length
+ console.log(average)
